c65faa5e9499fb145ebbed8a60ea89d3f9699599
marciacristinaamorim <97485509+marciacristinaamorim@users.noreply.github.com>
2022-04-04 10:46:47 -0300

    Avaliação

---
+++ instruções.js
@@ -0,0 +1,72 @@
+function usoMicroondas (elemento, tempo){
+    if (elementoPipoca < tempo 10 segundos){
+        console.log (Tempo Insuficiente);
+    }
+    if (elementoPipoca=> 20 segundos && elementoPipoca < 30){
+        console.log (Queimou a comida);
+    }
+    if (elementoPipoca=< 30 segundos){
+        console.log (Kabum);
+    } 
+    else if (elementoPipoca=== finalizado){
+        console.log (Prato pronto, bom apetite!!!)
+    }
+    if (elementoMacarrao> tempo 8 segundos){
+        console.log (Tempo Insuficiente);
+    }
+    if (elementoMacarrao> tempo 8 segundos && elementoMacarrao < 16){
+        console.log (Queimou a comida);
+    }
+    if (elementoMacarrao=> 16 segundos){
+        console.log (Kabum);
+    }
+    else if (elementoMacarrao === finalizado){
+        console.log (Prato pronto, bom apetite!!!);
+    }
+    if (elementoCarne> tempo 15 segundos && elementoCarne < 30){
+        console.log (Tempo Insuficiente);
+    }
+    if (elementoCarne=> 30 segundos){
+        console.log (Queimou a comida);
+    }
+    if (elementoCarne=< 45 segundos){
+        console.log (Kabum);
+    }
+    else if (elementoCarne=== finalizado){
+        console.log (Prato pronto, bom apetite!!!);
+    }
+    if (elementoFeijao=> tempo 12 segundos && elementoFeijao < 30){
+        console.log (Tempo Insuficiente);
+    }
+    if (elementoFeijao=> 24 segundos){
+        console.log (Queimou a comida);
+    }
+    if (elementoFeijao=< 36 segundos){
+        console.log (Kabum);
+    }
+    else if (elementoFeijao === finalizado){
+        console.log (Prato pronto, bom apetite!!!);
+    }
+    if (elementoBrigadeiro> tempo 8 segundos && elementoBrigadeiro < 30){
+        console.log (Tempo Insuficiente);
+    }
+    if (elementoBrigadeiro=> 16 segundos){
+        console.log (Queimou a comida);
+    }
+    if (elementoBrigadeiro=< 24 segundos) {
+        console.log (Kabum);
+    }
+    else if (elementoBrigadeiro === finalizado){
+        console.log (Prato pronto, bom apetite!!!);
+    }
+    if (elementoAbobrinha === Desconhecido){
+        console.log (Prato Inexistente);
+    }
+    }
+    usoMicroondas (Pipoca,10)
+    usoMicroondas (Macarrao, 8)
+    usoMicroondas (Carne,15)
+    usoMicroondas (Feijao, 12)
+    usoMicroondas (Brigadeiro,8)
+    usoMicroondas (Abobrinha,Desconhecido)
+    
